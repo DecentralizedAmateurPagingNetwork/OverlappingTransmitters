12d40437c5a92ec598a2ed95d23ef92658290609
Ralf Wilke <wilke@ihf.rwth-aachen.de>
2018-03-26 10:04:27 +0200

    Added jar output

--- src/OverlappingTransmitters/Main.java
+++ src/OverlappingTransmitters/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
     Transmitters Ts = new Transmitters();
-    Ts.readAllTransmittersMetaData("Coverage");
+    Ts.readAllTransmittersMetaData("Coverage_all");
     Ts.findPotentialOverlappingTransmitters();
     Ts.printPotentialOverlappingTransmitters();
     Ts.verifyOverlapping();
